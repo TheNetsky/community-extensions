23fdef2c8e97457b7f210a11286fa4b914ec194d
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-04-19 15:37:51 +0000

    Deploying to gh-pages from @ TheNetsky/community-extensions@944fd3d5c5426d638888cac4f5b84ff6685f34c4 🚀

--- 0.8/NHentai/source.js
+++ 0.8/NHentai/source.js
@@ -465,9 +465,10 @@ const types_1 = require("@paperback/types");
 const NHentaiHelper_1 = require("./NHentaiHelper");
 const NHentaiParser_1 = require("./NHentaiParser");
 const NHentaiSettings_1 = require("./NHentaiSettings");
+const tags_json_1 = require("./tags.json");
 const NHENTAI_URL = 'https://nhentai.net';
 exports.NHentaiInfo = {
-    version: '4.0.0',
+    version: '4.0.1',
     name: 'nhentai',
     icon: 'icon.png',
     author: 'NotMarek & Netsky',
@@ -553,6 +554,16 @@ class NHentai {
         const jsonData = this.parseJson(response);
         return (0, NHentaiParser_1.parseChapterDetails)(jsonData, mangaId);
     }
+    async getSearchTags() {
+        const arrayTags = [];
+        for (const tag of tags_json_1.popularTags) {
+            const label = tag.label;
+            const id = tag.id;
+            arrayTags.push({ id: id, label: label });
+        }
+        const tagSections = [App.createTagSection({ id: '0', label: 'Tags', tags: arrayTags.map(x => App.createTag(x)) })];
+        return tagSections;
+    }
     async getSearchResults(query, metadata) {
         const page = metadata?.page ?? 1;
         const title = query.title ?? '';
@@ -583,10 +594,9 @@ class NHentai {
             // Normal search query
         }
         else {
-            const q = title + ' ' + this.generateQuery();
-            const [sort, query] = await this.sortOrder(q, this.stateManager) ?? ['', q];
+            const q = encodeURIComponent(`${title} ${query?.includedTags?.map((x) => ` +${x.id}`)} `) + await this.generateQuery();
             const request = App.createRequest({
-                url: `${NHENTAI_URL}/api/galleries/search?query=${encodeURIComponent(query ?? '')}&sort=${sort}&page=${page}`,
+                url: `${NHENTAI_URL}/api/galleries/search?query=${(q)}&page=${page}&sort=${await this.sortOrder(this.stateManager)}`,
                 method: 'GET'
             });
             const response = await this.requestManager.schedule(request, 1);
@@ -719,15 +729,9 @@ class NHentai {
             return `language:${lang}`;
         }
     }
-    async sortOrder(query, stateManager) {
-        const inQuery = NHentaiHelper_1.NHSortOrders.containsShortcut(query);
-        if (inQuery[0]?.length !== 0) {
-            return [inQuery[0] ?? '', query.replace(inQuery[1] ?? '', '')];
-        }
-        else {
-            const sortOrder = await stateManager.retrieve('sort_order') ?? NHentaiHelper_1.NHSortOrders.getDefault();
-            return [sortOrder, query];
-        }
+    async sortOrder(stateManager) {
+        const sortOrder = await stateManager.retrieve('sort_order') ?? NHentaiHelper_1.NHSortOrders.getDefault();
+        return sortOrder;
     }
     async extraArgs(stateManager) {
         const args = await (0, NHentaiSettings_1.getExtraArgs)(stateManager);
@@ -736,7 +740,7 @@ class NHentai {
 }
 exports.NHentai = NHentai;
 
-},{"./NHentaiHelper":63,"./NHentaiParser":64,"./NHentaiSettings":65,"@paperback/types":61}],63:[function(require,module,exports){
+},{"./NHentaiHelper":63,"./NHentaiParser":64,"./NHentaiSettings":65,"./tags.json":66,"@paperback/types":61}],63:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NHSortOrders = exports.NHLanguages = void 0;
@@ -849,8 +853,9 @@ const parseMangaDetails = (data) => {
     const artist = getArtist(data);
     const tags = [];
     for (const tag of data.tags) {
-        if (tag.type === 'tag')
-            tags.push(App.createTag({ id: tag.id.toString(), label: tag.name }));
+        if (tag.type !== 'tag')
+            continue;
+        tags.push(App.createTag({ id: tag.name, label: capitalizeTags(tag.name) }));
     }
     return App.createSourceManga({
         id: data.id.toString(),
@@ -889,18 +894,27 @@ const parseChapterDetails = (data, mangaId) => {
 exports.parseChapterDetails = parseChapterDetails;
 const parseSearch = (data) => {
     const tiles = [];
+    const collectedIds = [];
     for (const gallery of data.result) {
+        if (collectedIds.includes(gallery.id.toString()))
+            continue;
         tiles.push(App.createPartialSourceManga({
             image: `https://t.nhentai.net/galleries/${gallery.media_id}/cover.${typeOfImage(gallery.images.cover)}`,
             title: gallery.title.pretty,
             mangaId: gallery.id.toString(),
             subtitle: NHentaiHelper_1.NHLanguages.getName(getLanguage(gallery))
         }));
+        collectedIds.push(gallery.id.toString());
     }
     return tiles;
 };
 exports.parseSearch = parseSearch;
 // Utility
+function capitalizeTags(str) {
+    return str.split(' ').map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+}
 const typeMap = { 'j': 'jpg', 'p': 'png', 'g': 'gif' };
 const typeOfImage = (image) => {
     return typeMap[image.t] ?? '';
@@ -1013,5 +1027,247 @@ const resetSettings = (stateManager) => {
 };
 exports.resetSettings = resetSettings;
 
-},{"./NHentaiHelper":63}]},{},[62])(62)
+},{"./NHentaiHelper":63}],66:[function(require,module,exports){
+module.exports={
+    "popularTags": [
+        {
+            "id": "big-breasts",
+            "label": "Big Breasts"
+        },
+        {
+            "id": "solo-female",
+            "label": "Solo Female"
+        },
+        {
+            "id": "sole-male",
+            "label": "Solo Male"
+        },
+        {
+            "id": "anal",
+            "label": "Anal"
+        },
+        {
+            "id": "stockings",
+            "label": "Stockings"
+        },
+        {
+            "id": "schoolgirl-uniform",
+            "label": "Schoolgirl Uniform"
+        },
+        {
+            "id": "nakadashi",
+            "label": "Nakadashi"
+        },
+        {
+            "id": "blowjob",
+            "label": "Blowjob"
+        },
+        {
+            "id": "bondage",
+            "label": "Bondage"
+        },
+        {
+            "id": "mosaic-censorship",
+            "label": "Mosaic Censorship"
+        },
+        {
+            "id": "ahegao",
+            "label": "Ahegao"
+        },
+        {
+            "id": "males-only",
+            "label": "Males Only"
+        },
+        {
+            "id": "dark-skin",
+            "label": "Dark Skin"
+        },
+        {
+            "id": "double-penetration",
+            "label": "Double Penetration"
+        },
+        {
+            "id": "tankoubon",
+            "label": "Tankoubon"
+        },
+        {
+            "id": "futanari",
+            "label": "Futanari"
+        },
+        {
+            "id": "defloration",
+            "label": "Defloration"
+        },
+        {
+            "id": "multi-work-series",
+            "label": "Multi Work Series"
+        },
+        {
+            "id": "sex-toys",
+            "label": "Sex Toys"
+        },
+        {
+            "id": "swimsuit",
+            "label": "Swimsuit"
+        },
+        {
+            "id": "ffm-threesome",
+            "label": "FFM Threesome"
+        },
+        {
+            "id": "full-censorship",
+            "label": "Full Censorship"
+        },
+        {
+            "id": "femdom",
+            "label": "Femdom"
+        },
+        {
+            "id": "dilf",
+            "label": "DILF"
+        },
+        {
+            "id": "twintails",
+            "label": "Twintails"
+        },
+        {
+            "id": "pantyhose",
+            "label": "Pantyhose"
+        },
+        {
+            "id": "sister",
+            "label": "Sister"
+        },
+        {
+            "id": "crossdressing",
+            "label": "Crossdressing"
+        },
+        {
+            "id": "tentacles",
+            "label": "Tentacles"
+        },
+        {
+            "id": "mind-break",
+            "label": "Mind Break"
+        },
+        {
+            "id": "bikini",
+            "label": "Bikini"
+        },
+        {
+            "id": "schoolboy-uniform",
+            "label": "Schoolboy Uniform"
+        },
+        {
+            "id": "story-arc",
+            "label": "Story Arc"
+        },
+        {
+            "id": "tomgirl",
+            "label": "Tomgirl"
+        },
+        {
+            "id": "big-ass",
+            "label": "Big Sss"
+        },
+        {
+            "id": "sweating",
+            "label": "Sweating"
+        },
+        {
+            "id": "mmf-threesome",
+            "label": "MMF Threesome"
+        },
+        {
+            "id": "teacher",
+            "label": "Teacher"
+        },
+        {
+            "id": "exhibitionism",
+            "label": "Exhibitionism"
+        },
+        {
+            "id": "uncensored",
+            "label": "Uncensored"
+        },
+        {
+            "id": "females-only",
+            "label": "Females Only"
+        },
+        {
+            "id": "lingerie",
+            "label": "Lingerie"
+        },
+        {
+            "id": "unusual-pupils",
+            "label": "Unusual Pupils"
+        },
+        {
+            "id": "footjob",
+            "label": "Footjob"
+        },
+        {
+            "id": "mother",
+            "label": "Mother"
+        },
+        {
+            "id": "huge-breasts",
+            "label": "Huge-breasts"
+        },
+        {
+            "id": "gender-bender",
+            "label": "Gender Bender"
+        },
+        {
+            "id": "catgirl",
+            "label": "Catgirl"
+        },
+        {
+            "id": "demon-girl",
+            "label": "Demon Girl"
+        },
+        {
+            "id": "kimono",
+            "label": "Kimono"
+        },
+        {
+            "id": "prostitution",
+            "label": "Prostitution"
+        },
+        {
+            "id": "stomach-deformation",
+            "label": "Stomach Deformation"
+        },
+        {
+            "id": "horns",
+            "label": "Horns"
+        },
+        {
+            "id": "webtoon",
+            "label": "Webtoon"
+        },
+        {
+            "id": "monster-girl",
+            "label": "Monster Girl"
+        },
+        {
+            "id": "latex",
+            "label": "Latex"
+        },
+        {
+            "id": "fox-girl",
+            "label": "Fox Girl"
+        },
+        {
+            "id": "yandere",
+            "label": "Yandere"
+        },
+        {
+            "id": "milking",
+            "label": "Milking"
+        }
+    ]
+}
+
+},{}]},{},[62])(62)
 });
