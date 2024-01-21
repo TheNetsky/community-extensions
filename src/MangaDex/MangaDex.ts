292afe210689b80d3fedd0d46bc5983ce630798e
Nicholas Lim <nicknitewolf@lineageos.org>
2024-01-21 16:58:57 +0800

    MangaDex | 3.0.4

--- src/MangaDex/MangaDex.ts
+++ src/MangaDex/MangaDex.ts
@@ -64,7 +64,7 @@ export const MangaDexInfo: SourceInfo = {
     description: 'Extension that pulls manga from MangaDex',
     icon: 'icon.png',
     name: 'MangaDex',
-    version: '3.0.3',
+    version: '3.0.4',
     authorWebsite: 'https://github.com/nar1n',
     websiteBaseURL: MANGADEX_DOMAIN,
     contentRating: ContentRating.EVERYONE,
