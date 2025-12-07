3f20e29729f2be8ac6136486c8600564403cc00f
Frey <41562938+OTCompa@users.noreply.github.com>
2025-12-07 09:29:32 -0600

    MangaDex | Fix title logic (#74)
    The title was undefined for certain manga because the default title language for that series was something other than `en` (e.g `ja-ro`) and there were no alternate titles for the series.

--- src/MangaDex/MangaDexParser.ts
+++ src/MangaDex/MangaDexParser.ts
@@ -9,7 +9,7 @@ export const parseMangaList = async (object: MangaItem[], source: any, thumbnail
     for (const manga of object) {
         const mangaId = manga.id
         const mangaDetails = manga.attributes
-        const title = source.decodeHTMLEntity(mangaDetails.title.en ?? mangaDetails.altTitles.map(x => Object.values(x).find((v) => v !== undefined)).find((t) => t !== undefined))
+        const title = source.decodeHTMLEntity(mangaDetails.title.en ?? Object.values(mangaDetails.title)[0] ?? mangaDetails.altTitles.map(x => Object.values(x).find((v) => v !== undefined)).find((t) => t !== undefined)) ?? 'INVALID TITLE'
         const coverFileName = manga.relationships.filter((x) => x.type == 'cover_art').map((x) => x.attributes?.fileName)[0]
         const image = coverFileName ? `${source.COVER_BASE_URL}/${mangaId}/${coverFileName}${MDImageQuality.getEnding(await thumbnailSelector(source.stateManager))}` : 'https://mangadex.org/_nuxt/img/cover-placeholder.d12c3c5.jpg'
         const subtitle = `${mangaDetails.lastVolume ? `Vol. ${mangaDetails.lastVolume}` : ''} ${mangaDetails.lastChapter ? `Ch. ${mangaDetails.lastChapter}` : ''}`
